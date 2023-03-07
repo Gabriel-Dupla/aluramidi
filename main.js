680d83075e6e173b466512a06a5fc0ff5e3483cf
Gabriel-Dupla <gabrielduplacontato@gmail.com>
2023-03-07 12:05:20 -0300

    Teclas enter e  numpadenter ativam os sons das teclas

--- main.js
+++ main.js
@@ -9,12 +9,25 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-  
     const idAudio = `#som_${instrumento}`;
-
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-    };
+    }
+
+    
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter' || evento.code === 'NumpadEnter') {
+            tecla.classList.add('ativa');
+
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+		tecla.classList.remove('ativa');
+}
+
 
 }
